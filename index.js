6612ffe89501c70d98febde9e8cc62dcc896b795
Jon Eisen <jon@metricfeat.com>
2015-09-20 23:18:01 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,51 @@
+var args = require('minimist')(process.argv.slice(2))
+  , request = require('request')
+  , async = require('async')
+  , Etcd = require('node-etcd')
+  , api_key = args.api_key
+  , etcd = new Etcd(args.etcd ? args.etcd.split(',') : null)
+  , prefix = args.prefix || '/skydns/'
+  , interval = args.interval || 300000
+
+setInterval(function () {
+  listLinodes(updateDNS)
+}, interval)
+
+listLinodes(updateDNS)
+
+function updateDNS(records) {
+  records.forEach(function (pair) {
+    var ip = pair[0]
+      , name = pair[1]
+
+    console.log('Setting DNS for ' + name + ': ' + ip)
+
+    etcd.set(prefix + name, {host: ip})
+  })
+}
+
+function listLinodes(callback) {
+  async.parallel([
+    apiGet.bind(null, 'linode.list'),
+    apiGet.bind(null, 'linode.ip.list')
+  ], function (_, lists) {
+    var labels = lists[0].DATA.reduce(function (a, n) { a[n.LINODEID] = n.LABEL; return a }, {})
+      , records = lists[1].DATA.filter(function (ip) { return !ip.ISPUBLIC })
+          .map(function (ip) {
+            return [ip.IPADDRESS, labels[ip.LINODEID]]
+          })
+
+    callback(records)
+  })
+}
+
+function apiGet(action, cb) {
+  request.get({
+    uri: 'https://api.linode.com/?api_action='+action+'&api_key='+api_key,
+    json: true
+  }, function (err, res, body) {
+    if (err) throw err
+    if (!body) throw new Error('no body on ' + action + ' call')
+    cb(null, body)
+  })
+}
